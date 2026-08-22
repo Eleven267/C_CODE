05491497226bb792aedf0ef8d354a25dba285a9b
elevennn_n <2661931249@qq.com>
2026-08-22 08:41:02 +0800

    单链表

---
+++ Slist/Slist/SList.c
@@ -0,0 +1,211 @@
+﻿#define _CRT_SECURE_NO_WARNINGS 1
+#include"SList.h"
+
+//链表打印
+void SLTPrint(SLTNode* phead)
+{
+	SLTNode* pcur = phead;
+	while (pcur)
+	{
+		printf("%d->", pcur->data);
+		pcur = pcur->next;
+	}
+	printf("NULL\n");
+}
+
+
+//创建一个节点
+SLTNode* SLTBuyNode(SLTDataType x)
+{
+	SLTNode* newnode = (SLTNode*)malloc(sizeof(SLTNode));
+	if (newnode == NULL)
+	{
+		printf("malloc fail!");
+		exit(1);
+	}
+	newnode->data = x;
+	newnode->next = NULL;
+
+	return newnode;
+}
+
+
+//尾插
+void SLTPushBack(SLTNode** pphead, SLTDataType x)
+{
+	assert(pphead);
+	SLTNode* newnode = SLTBuyNode(x);
+	//两种情况：空链表，非空链表
+	//1.空链表
+	if (*pphead == NULL)
+	{
+		*pphead = newnode;
+	}
+	//2.非空链表
+	else
+	{
+		//找尾
+		SLTNode* ptail = *pphead;
+		while (ptail->next)
+		{
+			ptail = ptail->next;
+		}
+		//ptail指向尾节点 
+		ptail->next = newnode;
+	}
+
+}
+
+
+//头插
+void SLTPushFront(SLTNode** pphead, SLTDataType x)
+{
+	assert(pphead);
+	SLTNode* newnode = SLTBuyNode(x);
+	newnode->next = *pphead;
+	*pphead = newnode;
+}
+
+
+//尾删
+void SLTPopBack(SLTNode** pphead)
+{
+	assert(pphead && *pphead);
+	SLTNode* prev = *pphead;
+	SLTNode* ptail = *pphead;
+	//1.链表只有一个节点
+	if ((*pphead)->next == NULL)
+	{
+		free(*pphead);
+		*pphead = NULL;
+	}
+	//2.链表有多个节点
+	else
+	{
+		while (ptail->next)
+		{
+			prev = ptail;
+			ptail = ptail->next;
+		}
+		free(ptail);
+		ptail = NULL;
+		prev->next = NULL;
+	}
+}
+
+
+//头删
+void SLTPopFront(SLTNode** pphead)
+{
+	assert(pphead && *pphead);
+	SLTNode* next = (*pphead)->next;
+	free(*pphead);
+	*pphead = next;
+}
+
+
+//查找
+SLTNode* SLTFind(SLTNode* phead, SLTDataType x)
+{
+	SLTNode* pcur = phead;
+	while (pcur)
+	{
+		if (pcur->data == x)
+		{
+			return pcur;
+		}
+		else
+		{
+			pcur = pcur->next;
+		}
+	}
+	return NULL;
+}
+
+
+//在指定位置之前插⼊数据
+void SLTInsert(SLTNode** pphead, SLTNode* pos, SLTDataType x)
+{
+	assert(pphead && *pphead);
+	assert(pos);
+	SLTNode* newnode = SLTBuyNode(x);
+
+	if (pos == *pphead)
+	{
+		SLTPushFront(pphead, x);
+	}
+	else
+	{
+		SLTNode* prev = *pphead;
+		while (prev->next != pos)
+		{
+			prev = prev->next;
+		}
+		prev->next = newnode;
+		newnode->next = pos;
+	}
+}
+//在指定位置之后插⼊数据
+void SLTInsertAfter(SLTNode* pos, SLTDataType x)
+{
+	assert(pos);
+	SLTNode* newnode = SLTBuyNode(x);
+
+	newnode->next = pos->next;
+	pos->next = newnode;
+
+}
+
+
+//删除pos节点
+void SLTErase(SLTNode** pphead, SLTNode* pos)
+{
+	assert(*pphead && pphead);
+	assert(pos);
+
+	//pos是头节点
+	if (pos == *pphead)
+	{
+		//头删
+		SLTPopFront(pphead);
+	}
+	//pos不是节点
+	else
+	{
+		SLTNode* prev = *pphead;
+		while (prev->next != pos)
+		{
+			prev = prev->next;
+		}
+		prev->next = pos->next;
+		free(pos);
+		pos = NULL;
+	}
+}
+
+
+//删除pos之后的节点
+void SLTEraseAfter(SLTNode* pos)
+{
+	assert(pos && pos->next);
+	SLTNode* del = pos->next;
+	pos->next = del->next;
+	free(del);
+	del = NULL;
+}
+
+
+//销毁链表
+void SListDesTroy(SLTNode** pphead)
+{
+	assert(pphead && *pphead);
+
+	SLTNode* pcur = *pphead;
+	while (pcur)
+	{
+		SLTNode* next = pcur->next;
+		free(pcur);
+		pcur = next;
+	}
+	*pphead = NULL;
+}
